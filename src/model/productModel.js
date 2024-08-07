e69c6f23ec1cbc3c12f0d1d307d50160f85eabb0
Ranjeetvit453 <66463444+Ranjeetvit453@users.noreply.github.com>
2024-08-07 08:02:50 +0530

    Update productModel.js

--- src/model/productModel.js
+++ src/model/productModel.js
@@ -1,11 +1,9 @@
 const mongoose =require("mongoose");
 
-
-
 const productSchema = new mongoose.Schema({
 productName :{type:String,required: true},
 productDescription:{type:String,required: true},
-image:{type:String,required: true},
+imageUrl:{type:String,required: true},
 price:{type:Number,requeid:true}
 })
 
