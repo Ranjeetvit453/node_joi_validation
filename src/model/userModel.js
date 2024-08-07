f1f0e28a794d10aabaea71a5c39087f142fc2ecd
Ranjeetvit453 <66463444+Ranjeetvit453@users.noreply.github.com>
2024-08-07 08:07:02 +0530

    Update userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
     email:{type:String,required:true},
     password:{type:String,required:true},
     mobile:{type:String,required:true},
+    address:{type:String,required:true},
 
 })
 
