74b1d4023edfc3493d43a650d225f07dfe70ec44
Ranjeetvit453 <66463444+Ranjeetvit453@users.noreply.github.com>
2024-08-08 08:36:11 +0530

    Update utils.js

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -3,7 +3,7 @@ const saltRounds = 10;
 const bcrypt = require('bcrypt');
 module.exports = class Utils{
     static async verifiyCode(data){
-       return "ranjeetkumar"+data
+       return "ranjeetkumar"
     }
     static async generate(data){
         try{
